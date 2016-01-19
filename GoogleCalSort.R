2337c0f7df20f294357d9aa461dea46dbf87c07d
Eric Youngson <eric@scneco.com>
2016-01-18 20:02:08 -0800

    Added call to 'lubridate' package, modified sortGglCal function to convert Date field to R Date-type

--- GoogleCalSort.R
+++ GoogleCalSort.R
@@ -4,6 +4,8 @@
 # __license__ = "GNU Affero (GPLv3)"
 # 
 
+# install.packages("lubridate") ## Comment out after first call to source()
+library("lubridate")
 
 sortGglCal <- function(f.name){
   # Reads exported calendar event data from Google Calendar and return a
@@ -16,6 +18,7 @@ sortGglCal <- function(f.name){
   #   A dataframe containing calendar data parameters, with useful R datatypes
   #   for event categories
   df = read.csv(f.name, header = TRUE, stringsAsFactors =FALSE)
+  df$Date = as.Date(df$Date, format = '%m/%d/%y')
 
   return(df)
 }
